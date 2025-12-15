a180cc1ef23242ca77cb2723e5ad18ec61faf58e
azrng <itzhangyunpeng@163.com>
2025-12-15 22:09:12 +0800

    feat:core增加将对象转换为目标类型的值

--- src/Shared/Azrng.Core/Helpers/ObjectHelper.cs
+++ src/Shared/Azrng.Core/Helpers/ObjectHelper.cs
@@ -49,5 +49,76 @@ namespace Azrng.Core.Helpers
 
             return result;
         }
+
+        /// <summary>
+        /// 将对象转换为目标类型的值
+        /// </summary>
+        public static object ConvertToTargetType(object value, Type targetType)
+        {
+            if (value is null)
+                throw new ArgumentNullException(nameof(value));
+            if (targetType is null)
+                throw new ArgumentNullException(nameof(targetType));
+
+            // 处理可空类型
+            if (targetType.IsGenericType && targetType.GetGenericTypeDefinition() == typeof(Nullable<>))
+            {
+                // 获取可空类型的实际类型
+                targetType = Nullable.GetUnderlyingType(targetType);
+            }
+
+            if (targetType == typeof(int))
+            {
+                return Convert.ToInt32(value);
+            }
+            else if (targetType == typeof(long))
+            {
+                return Convert.ToInt64(value);
+            }
+            else if (targetType == typeof(decimal))
+            {
+                return Convert.ToDecimal(value);
+            }
+            else if (targetType == typeof(double))
+            {
+                return Convert.ToDouble(value);
+            }
+            else if (targetType == typeof(float))
+            {
+                return Convert.ToSingle(value);
+            }
+            else if (targetType == typeof(string))
+            {
+                return value?.ToString() ?? string.Empty;
+            }
+            else if (targetType == typeof(DateTime))
+            {
+                return Convert.ToDateTime(value);
+            }
+            else if (targetType == typeof(bool))
+            {
+                return Convert.ToBoolean(value);
+            }
+            else if (targetType!.IsEnum)
+            {
+                if (value is string stringValue)
+                    return Enum.Parse(targetType, stringValue);
+                else
+                    return Enum.ToObject(targetType, Convert.ToInt32(value));
+            }
+            else
+            {
+                // 默认使用 Convert.ChangeType 转换
+                try
+                {
+                    return Convert.ChangeType(value, targetType);
+                }
+                catch
+                {
+                    // 如果转换失败，返回原始值
+                    return value;
+                }
+            }
+        }
     }
 }
