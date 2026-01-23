2c7e372f03ff5db8ed5588929eb78c1a35a0277d
azrng <zhang.yunpeng@synyi.com>
2026-01-23 14:20:25 +0800

    feat:json 增加CustomContractResolver

---
+++ src/Shared/Azrng.Core.NewtonsoftJson/ContractResolvers/CustomContractResolver.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Azrng.Core.NewtonsoftJson.ContractResolvers
+{
+    /// <summary>
+    /// 自定义契约解析器
+    /// </summary>
+    public class CustomContractResolver : CamelCasePropertyNamesContractResolver
+    {
+        /// <summary>将long类型转字符串</summary>
+        /// <param name="objectType"></param>
+        /// <returns></returns>
+        protected override JsonConverter? ResolveContractConverter(Type objectType)
+        {
+            return objectType == typeof (long) ? new JsonConverterLong() : base.ResolveContractConverter(objectType);
+        }
+    }
+}
